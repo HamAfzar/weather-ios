b19faacf1d6d277ec06e26c206b070ee56874722
amirhossein pooya <amirhosseinpooya@gmail.com>
2020-03-21 14:15:01 +0430

    init project

---
+++ WeatherMether/WeatherMether/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherMether
+//
+//  Created by amirhosseinpy on 1/2/1399 AP.
+//  Copyright © 1399 WhiteOwl. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
