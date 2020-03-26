06890190952c51bf0fbd03dd14f83a17488b6d78
amirhossein pooya <amirhosseinpooya@gmail.com>
2020-03-26 15:16:37 +0430

    add network layer

---
+++ WeatherMether/WeatherMether/Netwoork/NetworkError.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkError.swift
+//  WeatherMether
+//
+//  Created by amirhosseinpy on 1/7/1399 AP.
+//  Copyright © 1399 WhiteOwl. All rights reserved.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+  case parsing(description: String)
+  case network(description: String)
+}
