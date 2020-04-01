4cc52d9def070c76cadc8395bf904e3232af2ad3
amirhossein pooya <amirhosseinpooya@gmail.com>
2020-04-01 17:47:48 +0430

    splash view finished

---
+++ WeatherMether/WeatherMether/Assets/ImageRefrences.swift
@@ -0,0 +1,9 @@
+//
+//  ImageRefrences.swift
+//  WeatherMether
+//
+//  Created by amirhosseinpy on 1/13/1399 AP.
+//  Copyright © 1399 Sepitmaan. All rights reserved.
+//
+
+import Foundation
---
+++ WeatherMether/WeatherMether/Splash/SplashView.swift
@@ -0,0 +1,21 @@
+//
+//  SplashView.swift
+//  WeatherMether
+//
+//  Created by amirhosseinpy on 1/13/1399 AP.
+//  Copyright © 1399 Sepitmaan. All rights reserved.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
